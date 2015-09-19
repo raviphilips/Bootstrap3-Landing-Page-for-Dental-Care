8e6a69781363f730820e80e0e381de28eaef6800
raviphilips <rakandula@hotmail.com>
2015-09-19 22:17:29 +0530

    First commit

---
+++ includes/js/script.js
@@ -0,0 +1,38 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
+
+
+			<script type="text/javascript">
+           	$(function () {
+                $('#datetimepicker1').datetimepicker();
+           		 });
+        	</script>
